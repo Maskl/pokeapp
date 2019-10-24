e0c3f8cf94054ed156d0a67b9b75502ee91ba248
Marek Sklodowski <marek.sklodowski@pearson.com>
2019-10-24 23:20:36 +0200

    Form component

--- src/app/features/home/form/form.component.ts
+++ src/app/features/home/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
-  constructor() {}
+  public searchForm: FormGroup = new FormGroup({
+    id: new FormControl(
+      '',
+      [
+        Validators.required,
+        Validators.pattern('^[1-9][0-9]*$')
+      ]
+    ),
+  });
+
+  public constructor(private router: Router) {}
+
+  public onFormSubmit(): void {
+    const chosenId: number = this.searchForm.get('id').value;
+
+    this.router.navigate([chosenId]);
+  }
 }
