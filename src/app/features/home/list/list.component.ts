f324053fffa3257f1b28879367855eabfd9a02cf
Marek Sklodowski <marek.sklodowski@pearson.com>
2019-10-25 00:12:44 +0200

    Pokemon list component

--- src/app/features/home/list/list.component.ts
+++ src/app/features/home/list/list.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokeApiService } from '../../../core/poke-api.service';
+import { Observable } from 'rxjs';
+import { IPokemonListData } from '../../../core/pokemon.model';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
-  public pokemonList: any;
+export class ListComponent implements OnInit {
+  public isLoading: boolean = true;
+  public pokemonList$: Observable<IPokemonListData[]>;
 
-  constructor(private pokeApiService: PokeApiService) {
-    this.pokemonList = pokeApiService.getPokemonList(1);
+  constructor(private pokeApiService: PokeApiService) {}
+
+  // TODO: Logic should be moved to service
+  public ngOnInit() {
+    this.isLoading = true;
+
+    this.pokemonList$ = this.pokeApiService
+      .getPokemonList(1)
+      .pipe(
+        finalize(() => this.isLoading = false)
+      );
   }
 }
